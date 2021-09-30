0836164f7e12f25c660c4871e425560a61878be9
Sanjeev Kumar Saini <sanjeevsaini11235@gmail.com>
2021-09-30 18:32:03 +0530

    Naming server ended

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "currency-exchange",url="localhost:8000")
+//@FeignClient(name = "currency-exchange",url="localhost:8000")
+@FeignClient(name = "currency-exchange")//will do load balancing for multiple instances
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
